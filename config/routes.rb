48e6649dcfdd21e2e60af6469f060a18c07a8c41
Ivan Hulko <ivanhulko14@gmail.com>
2022-07-04 13:26:08 +0300

    add first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :home_pages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home_pages#home'
 end
